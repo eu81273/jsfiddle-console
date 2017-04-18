056a6c9b083faaa48dcc912f057429b41e148ff2
aiden.ahn <aiden.ahn@kakaocorp.com>
2017-04-18 18:42:42 +0900

    Fix to show runtime error

--- console.js
+++ console.js
@@ -1,25 +1,53 @@
-! function (global) {
-	function append ($element) {
-		return document.body ? document.body.append($element) : setTimeout(append, 100, $element);
-	}
+(function () {
+    var _console = window.console;
+    var _methods = {
+        log: window.console.log,
+        error: window.console.error,
+        info: window.console.info,
+        debug: window.console.debug,
+        clear: window.console.clear,
+    };
+    var $body = document.body;
 
-	function element (text, color, $element) {
-		$element = document.createElement('p');
-		$element.style.color = color || '#000000';
-		$element.innerText = text;
-		return $element;
-	}
+    function append (message) {
+        $body.append(message);
+    }
 
-	function log () {
-		return append(element(Array.prototype.slice.call(arguments).join(' ')));
-	}
+    function clear () {
+        $body.innerHtml = "";
+        _methods.clear.call(_console);
+    };
 
-	function error () {
-		return append(element(Array.prototype.slice.call(arguments).join(' '), '#FF0000'));
-	}
+    function message (text, color, $message) {
+        $message = document.createElement('span');
+        $message.style.color = color || '#000000';
+        $message.innerText = text;
+        return $message;
+    }
 
-	global.console = {
-	   log: log,
-	   error: error
-	};
-} (window);
+    function write (key, color) {
+        return function () {
+            Function.prototype.apply.call(_methods[key], _console, arguments);
+            append(message(Array.prototype.slice.call(arguments).join(' '), color));
+        };
+    }
+
+    window.console.clear = clear;
+    window.console.error = write('error', '#ff0000');
+    window.console.log = write('log');
+    window.console.info = write('info');
+    window.console.debug = write('debug');
+
+    function errorHandler (e) {
+        e.preventDefault();
+        console.error(e.message);
+        return true;
+    }
+
+    if (window.attachEvent) {
+        window.attachEvent('onerror', errorHandler);
+    }
+    else {
+        window.addEventListener('error', errorHandler, true);
+    }
+}) ();
